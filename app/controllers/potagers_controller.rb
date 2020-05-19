7a5dee0f3c4ac8b8add86b4d0b37c3d7af56af0e
Harold <h.ngano@gmail.com>
2020-05-19 10:55:52 +0200

    debut controller potager

--- app/controllers/potagers_controller.rb
+++ app/controllers/potagers_controller.rb
@@ -8,4 +8,23 @@ def show
   @potager = Potagers.find(params[:id])
 end
 
+def new
+  @potager = Potagers.new
+end
+
+def create
+  @potager = Potagers.new(params[:potager])
+  if @potager.save
+    flash[:success] = "Votre potager est maintenant disponible"
+    redirect_to @potager
+  else
+    flash[:error] = "Quelque chose ne s'est pas passé comme prévu"
+    render 'new'
+  end
+end
+
+private
+
+
+
 end
