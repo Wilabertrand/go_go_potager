dfeb648bf608ae0e771cf5b11ef0ca6212126423
Astrid Boisson <astrid.bdc@gmail.com>
2020-05-19 18:46:02 +0200

    booking2 on progress

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :potagers
-  resources :bookings, only: [:index, :new, :create, :destroy]
+  resources :potagers do 
+    resources :bookings, only: [:new, :create]
+  end
+  resources :bookings, only: [:index, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
