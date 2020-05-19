7645d67cefd46de168a62d11d1a4b20a43d00ad6
Harold <h.ngano@gmail.com>
2020-05-19 13:02:59 +0200

    done

--- app/controllers/potagers_controller.rb
+++ app/controllers/potagers_controller.rb
@@ -1,30 +1,55 @@
 class PotagersController < ApplicationController
+  before_action :set_potager, only: [:edit, :update, :show, :destory]
 
-def index
-  @potagers = Potagers.all
-end
-
-def show
-  @potager = Potagers.find(params[:id])
-end
-
-def new
-  @potager = Potagers.new
-end
-
-def create
-  @potager = Potagers.new(params[:potager])
-  if @potager.save
-    flash[:success] = "Votre potager est maintenant disponible"
-    redirect_to @potager
-  else
-    flash[:error] = "Quelque chose ne s'est pas passé comme prévu"
-    render 'new'
+  def index
+    @potagers = Potager.all
   end
-end
 
-private
+  def show
+  end
+
+  def new
+    @potager = Potager.new
+  end
+
+  def create
+    if @potager.save(potager_params)
+      flash[:success] = "Votre potager est maintenant disponible"
+      redirect_to @potager
+    else
+      flash[:error] = "Quelque chose ne s'est pas passé comme prévu"
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @potager.update(potager_params)
+      flash[:success] = "Votre potager a été modifié"
+      redirect_to @potager
+    else
+      flash[:error] = "Quelque chose ne s'est pas passé comme prévu"
+      render 'new'
+    end
+  end
+
+  def destroy
+    @potager.destroy
+    flash[:success] = "Votre potager a été supprimé"
+    redirect_to @potagers
+  end
 
 
+  private
+
+  def set_potager
+    @potager = Potager.find(params[:id])
+  end
+
+  def potager_params
+    params.require(:potager).permit(:name, :address, :description, :price, :surface, :img_url, :user_id)
+  end
 
 end
