321d148e963708e2aa6dc8c298c94bdc814337e3
Wila Bertrand <wilaury.bertrand@acanopya.com>
2020-05-21 17:32:28 +0200

    problem

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
+Booking.destroy_all
+puts 'booking destroyed'
 Potager.destroy_all
 puts 'potager destroyed'
 User.destroy_all
