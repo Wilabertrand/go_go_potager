383a336ef322c3eb0aeaf9597c59ef380148bb8d
Wila Bertrand <wilaury.bertrand@acanopya.com>
2020-05-21 16:02:21 +0200

    Btn search and home

--- app/models/potager.rb
+++ app/models/potager.rb
@@ -1,8 +1,14 @@
 class Potager < ApplicationRecord
+  include PgSearch::Model
+  pg_search_scope :global_search,
+    against: [:address, :name],
+    using: {
+      tsearch: {prefix: true}
+    }
+
   belongs_to :user
 
   has_many :bookings
 
   has_one_attached :photo
-
 end
